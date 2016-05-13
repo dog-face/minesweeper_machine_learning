a81fc20b7d7ca837824fac1f68c6be948ef5daea
mznco <xterrain15@gmail.com>
2016-05-13 15:55:49 -0400

    now finding support vectors and alphas

--- src/Report_data/combined_validation.py
+++ src/Report_data/combined_validation.py
@@ -1,11 +1,23 @@
 import pickle
 import sys
+import numpy
 
 #Shows validation error of Generation and Gameplay based classifiers on a validation set of data from both types combined.
 
 
 def vectorize(data_pt):
     return data_pt.reshape(25).reshape(1, -1)
+
+def get_top_bottom(support_vectors, alphas):
+    blank1, top_10_indices = numpy.where(alphas == numpy.max(alphas))
+    blank2, bottom_10_indices = numpy.where(alphas == numpy.min(alphas))
+    top_10_vals = []
+    for index in top_10_indices:
+        top_10_vals.append(support_vectors[index])
+    bottom_10_vals = []
+    for index in bottom_10_indices:
+        bottom_10_vals.append(support_vectors[index])
+    return top_10_vals, bottom_10_vals, top_10_indices, bottom_10_indices
 
 generation_validation_set_data = pickle.load(open("../data/generation_validate_data.pickle", "rb"))
 generation_validation_set_keys = pickle.load(open("../data/generation_validate_keys.pickle", "rb"))
@@ -28,8 +40,11 @@ gameplay_support_vectors = gameplay_classifier.support_vectors_
 print("Gameplay classifier support vectors: " + str(len(gameplay_support_vectors)) + " support vectors")
 print(gameplay_support_vectors)
 
-gameplay_weights = generation_classifier.dual_coef_
-print("Gameplay classifier weights: \nt" + str(gameplay_weights))
+gameplay_alphas = gameplay_classifier.dual_coef_
+print("Gameplay classifier support vector weights: \n" + str(gameplay_alphas))
+
+print("Top and bottom support vectors: ")
+print(get_top_bottom(gameplay_support_vectors, gameplay_alphas))
 
 #Generation supp vecs and params
 generation_support_vectors = generation_classifier.support_vectors_
@@ -37,8 +52,11 @@ generation_support_vectors = generation_classifier.support_vectors_
 print("Generation classifier support vectors: " + str(len(generation_support_vectors)) + " support vectors")
 print(generation_support_vectors)
 
-generation_weights = generation_classifier.dual_coef_
-print("Generation classifier weights: \n" + str(generation_weights))
+generation_alphas = generation_classifier.dual_coef_
+print("Generation classifier support vector weights: \n" + str(generation_alphas))
+
+print("Top and bottom support vectors: ")
+print(get_top_bottom(generation_support_vectors, generation_alphas))
 
 #gameplay validation error
 print("Calculating validation error for gameplay classifier... ", end="")
